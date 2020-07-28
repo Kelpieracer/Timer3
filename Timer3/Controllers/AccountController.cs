d1d035c107237b6c3ca4da0d546fee01030ca8c8
T Mäntysalo <tapio@mantysalo.net>
2020-07-28 09:27:48 +0300

    Add project files.

---
+++ Timer3/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Timer3.Models;
+using Timer3.Services;
+
+namespace Timer3.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private AccountService AccountService { get; set; }
+
+        public AccountController(AccountService accountService)
+        {
+            AccountService = accountService;
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Login([FromBody] LoginModel model)
+        {
+            var result = AccountService.Login(HttpContext, model.Login, model.Password);
+            return Json(result);
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Logout()
+        {
+            var result = AccountService.Logout(HttpContext);
+            return Json(result);
+        }
+    }
+}
---
+++ Timer3/Infrastructure/ServiceUser.cs
@@ -0,0 +1,7 @@
+﻿namespace Timer3.Infrastructure
+{
+    public class ServiceUser
+    {
+        public string Login { get; set; }
+    }
+}
